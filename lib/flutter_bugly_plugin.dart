925d84adaa28b24dd350ce7982805a492ff3cc9c
fujianlian <lianbq@zz-med.com>
2019-11-20 15:58:50 +0800

    android bugly demo实现

--- lib/flutter_bugly_plugin.dart
+++ lib/flutter_bugly_plugin.dart
@@ -1,15 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class FlutterBuglyPlugin {
   static const MethodChannel _channel =
-      const MethodChannel('flutter_bugly_plugin');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  const MethodChannel('flutter_bugly_plugin');
 
   static void setUp(appID) {
     _channel.invokeMethod("setUp",{'app_id':appID});
